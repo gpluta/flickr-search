13d419293f8fa7b0544fde3088be05a35f8d05e0
Grzesiek Pluta <pluta7@gmail.com>
2016-11-26 21:15:03 +0100

    Move pagination and thumbs-list to separate components

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,14 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['search', 'page'],
+
   search: '',
+
   page: 1,
 
   nextLink: Ember.computed('page', function() {
     let candidate = parseInt(this.get('page')) + 1;
     let total = parseInt(this.get('model.photos.pages'));
 
-    return candidate >= total ? total : candidate;
+    return candidate >= total ? (total === 0 ? 1 : total) : candidate;
   }),
 
   prevLink: Ember.computed('page', function() {
@@ -18,14 +20,23 @@ export default Ember.Controller.extend({
     return candidate < 1 ? 1 : candidate;
   }),
 
+  /**
+   *  Convert total pages to int (from string)
+   */
+  totalElements: Ember.computed('model.photos.total', function() {
+    return parseInt(this.get('model.photos.total'));
+  }),
+
   modelIsLoading: true,
 
   actions: {
     executeSearch(e) {
-      this.setProperties({
-        search: e,
-        page: 1
-      });
+      if (e != '') {
+        this.setProperties({
+          search: e,
+          page: 1
+        });
+      }
     }
   }
 });
