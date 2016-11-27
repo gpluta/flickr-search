f7864d9a0ee1d94cd311f8e9851f6bd477a19da7
Grzesiek Pluta <pluta7@gmail.com>
2016-11-27 14:20:09 +0100

    Add some code comments

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // QP defined on this controller
   queryParams: ['search', 'page'],
 
+  // Properties holding values of the QPs
   search: '',
-
   page: 1,
 
+
+  /**
+   * CP returning number of the next page
+   */
   nextLink: Ember.computed('page', function() {
     let candidate = parseInt(this.get('page')) + 1;
     let total = parseInt(this.get('model.photos.pages'));
@@ -14,6 +19,9 @@ export default Ember.Controller.extend({
     return candidate >= total ? (total === 0 ? 1 : total) : candidate;
   }),
 
+  /**
+   * CP returning number of the previous page (unless is is not less than 1)
+   */
   prevLink: Ember.computed('page', function() {
     let candidate = parseInt(this.get('page')) - 1;
 
@@ -27,8 +35,14 @@ export default Ember.Controller.extend({
     return parseInt(this.get('model.photos.total'));
   }),
 
+  // A property updated from root, indicating if model is loading or if it has finished loading
+  // It is used to display the loading indicator.
   modelIsLoading: true,
 
+  /**
+   * Method executing search
+   * @param s - search string from search input
+   */
   executeSearch(s) {
     if (s != '') {
       this.setProperties({
@@ -39,10 +53,17 @@ export default Ember.Controller.extend({
   },
 
   actions: {
+    /**
+     * Action accepting a search string and passing it sto `executeSearch` controller method
+     * @param e - search string
+     */
     handleEnterOnSearchInput(e) {
       this.executeSearch(e);
     },
 
+    /**
+     * Action fired after clicking the search icon in the input.
+     */
     executeSearchWithSearchIcon() {
       let val = $('.flickr-search-input').val();
       if(val) {
